c9753b93d9e0ba8f39cab15a6bc16b0a2f7184e6
shotaseanyasuda <syasuda@students.kennesaw.edu>
2022-03-22 10:01:31 -0400

    added customer_info_entry.php file

---
+++ Customer/customer_info_entry.php
@@ -0,0 +1,77 @@
+<?php
+
+#include 'db_connection.php';
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "capstonedb";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+if(!$conn)
+{
+    die("Connection Failed: " . mysqli_connect_error());
+    echo "failed";
+}
+
+echo "connected successfully";
+
+$ksuid = (int)$_Post['ksuid'];
+$fname = $_Post['fname'];
+$lname = $_Post['lname'];
+$email = $_Post['email'];
+$phone = (int)$_Post['phone'];
+$request = time();
+
+echo "variables assigned";
+
+$sql_query = "INSERT INTO CUSTOMER (idCUSTOMER, Customer_Lname, Customer_Fname, KSU_Email,Phone_number,Date_of_request)
+VALUES($ksuid,'$lname','$fname','$email','$phone','$request')";
+
+
+
+if(mysqli_query($conn,$sql_query))
+{
+    echo "New Customer Info inserted successfully";
+}
+else
+{
+    echo "Error: " . $sql . "" . mysqli_error($conn);
+}
+mysqli_close($conn);
+
+/*
+
+if(isset($_Post['submit']))
+{
+    $fname = $_Post['fname'];
+    $lname = $_Post['lname'];
+    $email = $_Post['email'];
+    $phone = $_Post['phone'];
+    $request = $_Post['request'];
+
+    echo "variables assigned";
+
+    $sql_query = "INSERT INTO CUSTOMER (Customer_Lname, Customer_Fname, KSU_Email,Phone_number,Date_of_request)
+    VALUES('$lname','$fname','$email','$phone','$request')";
+
+    echo "querries ran";
+
+
+
+
+    if(mysqli_query($conn,$sql_query))
+    {
+        echo "New Customer Info inserted successfully";
+    }
+    else
+    {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+echo "didnt assign variables";
+
+*/
+
+?>
